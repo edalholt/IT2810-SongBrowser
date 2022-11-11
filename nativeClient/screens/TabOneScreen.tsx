dfbe5702bd95bb0f38994e4b2ead040a752f6ba5
Jakob Keller <jekeller@stud.ntnu.no>
2022-11-11 15:40:57 +0100

    add expand functionallity til each song in search result

--- nativeClient/screens/TabOneScreen.tsx
+++ nativeClient/screens/TabOneScreen.tsx
@@ -1,36 +1,41 @@
-import { useQuery, useReactiveVar } from '@apollo/client';
-import { StyleSheet } from 'react-native';
-import { ListItem, useTheme } from '@rneui/themed';
+import { useQuery, useReactiveVar } from "@apollo/client";
+import { StyleSheet, TouchableOpacity } from "react-native";
+import { Icon, ListItem, useTheme } from "@rneui/themed";
 
-import { songQueryVars } from '../GraphQL/cache';
-import { GET_SONGS } from '../GraphQL/Queries';
-import { RootTabScreenProps } from '../types';
-import { getSongsInputs, songsDataType } from '../types/songData';
-import { ScrollView, Text, View } from 'react-native';
+import { songQueryVars } from "../GraphQL/cache";
+import { GET_SONGS } from "../GraphQL/Queries";
+import { RootTabScreenProps } from "../types";
+import { getSongsInputs, songsDataType } from "../types/songData";
+import { ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import DropDownCard from "../components/DropDownCard";
 
-
-export default function TabOneScreen( { navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
   const songVars = useReactiveVar(songQueryVars);
-  const { theme, updateTheme } = useTheme()
+  const { theme, updateTheme } = useTheme();
+  const [expanded, setExpanded] = useState(false);
 
-  const { loading, error, data } = useQuery<songsDataType, getSongsInputs>(GET_SONGS, {
-    variables: songVars,
-  });
- 
-  if (error) return <Text style={styles.title}>There was an error. Is the backend running?</Text>
-  if (!data) return <Text style={styles.title}>Loading...</Text>
+  const { loading, error, data } = useQuery<songsDataType, getSongsInputs>(
+    GET_SONGS,
+    {
+      variables: songVars,
+    }
+  );
+
+  if (error)
+    return (
+      <Text style={styles.title}>
+        There was an error. Is the backend running?
+      </Text>
+    );
+  if (!data) return <Text style={styles.title}>Loading...</Text>;
   return (
-    <ScrollView style={{backgroundColor: theme.colors.primary}}>
-      {data.getSongs.songs.map(((song) => (
-        <>
-        <ListItem key={song._id} bottomDivider>
-          <ListItem.Content>
-            <ListItem.Title>{song.name}</ListItem.Title>
-            <ListItem.Subtitle>{song.year}</ListItem.Subtitle>
-          </ListItem.Content>
-        </ListItem>
-        </>
-      )))}
+    <ScrollView style={{ backgroundColor: theme.colors.primary }}>
+      {data.getSongs.songs.map((song) => (
+        <DropDownCard {...song} />
+      ))}
     </ScrollView>
   );
 }
@@ -38,14 +43,14 @@ export default function TabOneScreen( { navigation }: RootTabScreenProps<'TabOne
 const styles = StyleSheet.create({
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     padding: 25,
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '100%',
-    backgroundColor: 'yellow'
+    width: "100%",
+    backgroundColor: "yellow",
   },
 });
