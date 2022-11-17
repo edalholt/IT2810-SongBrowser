f861943997c8c43f44e1433d20f7add80cf15d85
Jakob Keller <jekeller@stud.ntnu.no>
2022-11-17 16:31:20 +0100

    add query for fetching songs in users songlist

--- nativeClient/GraphQL/Queries.ts
+++ nativeClient/GraphQL/Queries.ts
@@ -34,6 +34,25 @@ export const GET_SONGS = gql`
   }
 `;
 
+export const GET_USER_SONGS = gql`
+  query ($uid: String!) {
+    getUserSongs(uid: $uid) {
+      songs {
+        _id
+        name
+        artists
+        danceability
+        year
+        duration_ms
+        explicit
+        popularity
+        rating
+        isLiked
+      }
+    }
+  }
+`;
+
 export const LOGIN = gql`
   query ($username: String!, $password: String!) {
     login(username: $username, password: $password) {
--- nativeClient/components/ProfilePage.tsx
+++ nativeClient/components/ProfilePage.tsx
@@ -3,19 +3,38 @@ import React from "react";
 import { Text, TextInput, View } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import { isLoggedIn, songQueryVars } from "../GraphQL/cache";
-import { useReactiveVar } from "@apollo/client";
+import { useQuery, useReactiveVar } from "@apollo/client";
+import { GET_USER_SONGS } from "../GraphQL/Queries";
+import { userSongsDataType } from "../types/songData";
+import DropDownCard from "./DropDownCard";
 
 function ProfilePage() {
   const songVars = useReactiveVar(songQueryVars);
+
+  const { loading, error, data } = useQuery<userSongsDataType>(GET_USER_SONGS, {
+    variables: { uid: songVars.uid },
+  });
   const logout = () => {
     SecureStore.deleteItemAsync("token");
     isLoggedIn(false);
     songQueryVars({ ...songVars, uid: null });
   };
+  if (!data)
+    return (
+      <>
+        <Text>You have no songs in your list!</Text>
+        <Button title="Logout" type="solid" onPress={() => logout()} />
+      </>
+    );
 
   return (
     <>
-      <Text>profilePage</Text>
+      {data.getUserSongs.songs.map((song) => {
+        <View>
+          <Text>{song.name}</Text>
+        </View>;
+      })}
+
       <Button title="Logout" type="solid" onPress={() => logout()} />
     </>
   );
